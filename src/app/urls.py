dfd8658fd139c3c3cd11c73712b563f95bf0a3d9
askhat <askhat.9812@gmail.com>
2022-08-10 17:07:07 +0500

    Adding Errors Pages

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,12 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('shop-admin', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
+
+
+handler400 = 'shop.error_handlers.bad_request'
+handler403 = 'shop.error_handlers.permission_denied'
+handler404 = 'shop.error_handlers.not_found'
+handler500 = 'shop.error_handlers.server_error'
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
